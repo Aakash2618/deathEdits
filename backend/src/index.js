e014f0dc0f66b3b33aafc325d18f9b6fa4288a3b
Aakash <133749019+Aakash2618@users.noreply.github.com>
2025-10-14 00:06:15 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin:"http://localhost:5173/",
+  origin:"https://deathedits.vercel.app",
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 // app.use(cors())
